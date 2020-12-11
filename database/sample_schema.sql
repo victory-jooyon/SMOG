d838d2ea9309c88684210489c50a84219609bce5
victory-jooyon <gina611.kaist@gmail.com>
2020-12-12 00:43:31 +0900

    Add sample SQL files

---
+++ database/sample_schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE smog;
+
+USE smog;
+
+CREATE TABLE user
+(
+    age int NOT NULL,
+    class int NOT NULL,
+    id int PRIMARY KEY AUTO_INCREMENT
+);
---
+++ evaluation/test_queries.sql
@@ -0,0 +1,4 @@
+SELECT * FROM user WHERE (age > 10) AND (class = 5);
+SELECT * FROM user WHERE (class = 6) AND (age > 10);
+SELECT * FROM user WHERE (age <= 10) AND (class = 5);
+SELECT * FROM user WHERE (class = 6) AND (age > 10);
